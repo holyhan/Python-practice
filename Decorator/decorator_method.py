fe0190268ed74f687c8ae45c2cb1b15d00f74cae
holyhan <hanyang_k@sina.cn>
2018-06-05 23:39:34 +0800

    添加存储数据demo

--- Decorator/decorator_method.py
+++ Decorator/decorator_method.py
@@ -7,14 +7,16 @@ def method_friendly_decorator(method_to_decorator):
         return method_to_decorator(self, lie)
     return wrapper
 
+
 class Lucy:
 
     def __init__(self):
         self.age = 32
 
     @method_friendly_decorator
-    def sayYourAge(self, lie):
+    def say_your_age(self, lie):
         print("I am {}, what did you think?".format(self.age + lie))
 
+
 l = Lucy()
-l.sayYourAge(-3)
+l.say_your_age(-3)
--- HelloWorld/oop_subclass.py
+++ HelloWorld/oop_subclass.py
@@ -1,7 +1,8 @@
 # coding-UTF-8
 
+
 class SchoolMember:
-    '''代表任何学校里的成员。'''
+    """代表任何学校里的成员。"""
 
     def __init__(self, name, age):
         self.name = name
@@ -9,33 +10,29 @@ class SchoolMember:
         print('(Initialized SchoolMember: {})'.format(self.name))
 
     def tell(self):
-        '''告诉我有关我的细节'''
+        """告诉我有关我的细节"""
 
         print('Name:"{}" Age:"{}"'.format(self.name, self.age), end=" ")
 
 
-
 class Teacher(SchoolMember):
-    '''代表一位老师。'''
+    """代表一位老师。"""
     def __init__(self, name, age, salary):
         SchoolMember.__init__(self, name, age)
         self.salary = salary
         print('(Initialized Teacher: {})'.format(self.name))
-
 
     def tell(self):
         SchoolMember.tell(self)
         print('Salary: "{:d}"'.format(self.salary))
 
 
-
 class Student(SchoolMember):
-    '''代表一位学生。'''
+    """代表一位学生。"""
     def __init__(self, name, age, marks):
         SchoolMember.__init__(self, name, age)
         self.marks = marks
         print('(Initialized Student: {})'.format(self.name))
-
 
     def tell(self):
         SchoolMember.tell(self)
